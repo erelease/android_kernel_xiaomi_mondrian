0f66467dc12d68966d49c68e4f69d99dca1a2a51
zzh-zzh-zzh <1292882190@qq.com>
2023-02-10 23:09:44 +0800

    kernel/module.c: Ignore symbols crc check
    Change-Id: I1ec2933c8bf1ffd970a6c07fbe8b56636d67825f
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -1352,8 +1352,8 @@ static int check_version(const struct load_info *info,
 	return 1;
 
 bad_version:
-	pr_warn("%s: disagrees about version of symbol %s, but ignore...\n",
-	       info->name, symname);
+	pr_warn_once("%s: disagrees about version of symbol %s\n",
+			info->name, symname);
 	return 1;
 }
 
